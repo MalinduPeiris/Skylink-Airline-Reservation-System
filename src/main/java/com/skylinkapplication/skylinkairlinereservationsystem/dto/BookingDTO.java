866f7029f17ae317d811aef854b2aaf7b3ab1408
N H H D L De Silva <IT24103894@my.sliit.lk>
2025-09-17 09:22:45 +0530

    Adding BookingDTO class

---
+++ src/main/java/com/skylinkapplication/skylinkairlinereservationsystem/dto/BookingDTO.java
@@ -0,0 +1,38 @@
+package com.skylinkapplication.skylinkairlinereservationsystem.dto;
+
+import java.util.Date;
+
+public class BookingDTO {
+    private Long id;
+    private Date bookingDate;
+    private String status;
+    private Integer passengers;
+    private Long flightId;
+    private Long paymentId;
+
+
+    public BookingDTO() {}
+    public BookingDTO(Long id, Date bookingDate, String status, Integer passengers, Long flightId, Long paymentId) {
+        this.id = id;
+        this.bookingDate = bookingDate;
+        this.status = status;
+        this.passengers = passengers;
+        this.flightId = flightId;
+        this.paymentId = paymentId;
+    }
+
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public Date getBookingDate() { return bookingDate; }
+    public void setBookingDate(Date bookingDate) { this.bookingDate = bookingDate; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+    public Integer getPassengers() { return passengers; }
+    public void setPassengers(Integer passengers) { this.passengers = passengers; }
+    public Long getFlightId() { return flightId; }
+    public void setFlightId(Long flightId) { this.flightId = flightId; }
+    public Long getPaymentId() { return paymentId; }
+    public void setPaymentId(Long paymentId) { this.paymentId = paymentId; }
+}
+
